6a2cfa90dbf9db1aca42b8365fbd0f92ac2b4652
wendelsimoes <wendel.simoes.br@gmail.com>
2022-07-01 23:44:30 -0300

    feat: setup project structure

---
+++ app.js
@@ -0,0 +1,28 @@
+// Setup env var module
+require("dotenv").config({ path: __dirname + "/.env" });
+
+// Set express app
+const express = require("express");
+const app = express();
+
+// Default dev debugging port
+const port = parseInt(process.env.PORT);
+
+// Https module for call apis
+const https = require("https");
+
+app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=" +
+    process.env.OPEN_WEATHER_MAP_API_KEY;
+
+  https.get(url, function (response) {
+    console.log(response);
+  });
+
+  res.send("index");
+});
+
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+});
